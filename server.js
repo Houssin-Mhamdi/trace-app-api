9c3a00efd74f20604c7ea23df97394b313e555b6
houssinCarnelian <houssin.carnelian@gmail.com>
2025-04-23 02:30:16 +0100

    add start command

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const traceRoutes = require('./routes/traceRoutes');
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const traceRoutes = require("./routes/traceRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/traces', traceRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/traces", traceRoutes);
 
-mongoose.connect("mongodb+srv://houssinoss010203:ucxxx6BYZdJ9MTrn@cluster0.85ll9yz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log("MongoDB connected");
-  app.listen(5000, () => console.log("Server running on port 5000"));
-}).catch(err => console.log(err));
+mongoose
+  .connect(
+    process.env.MONGO_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT || 5000, () => console.log("Server running on port 5000"));
+  })
+  .catch((err) => console.log(err));
